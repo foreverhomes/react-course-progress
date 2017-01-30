8eb152c321d5d2b57dfc5076c9bcb31b25a54338
Jeff DeButte <jdebutte@league.com>
2017-01-30 09:50:21 -0500

    react progress

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCIVkZaznt9TKnIW_p4LzrRXYxQqkVv6Wg",
+	authDomain: "fish-store-d019f.firebaseapp.com",
+	databaseURL: "https://fish-store-d019f.firebaseio.com"
+})
+
+export default base
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+
+	goToStore(event) {
+		event.preventDefault()
+		this.context.router.transitionTo(`/store/${this.storeInput.value}`)
+	}
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store -></button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker
